d2dbfd8df542e732b3f5d6f2b39fd9f9f658dae6
datapirateX <67560590+datapirateX@users.noreply.github.com>
2022-12-08 01:51:02 +0600

    assets file

--- src/scenes/Landing.jsx
+++ src/scenes/Landing.jsx
@@ -1,8 +1,12 @@
-import useMediaQuery from "../hooks/useMediaQuery"
+import useMediaQuery from "../hooks/useMediaQuery";
+import { motion } from "framer-motion";
 const Landing = ({ setSelectedPage }) => {
     const isAboveLarge = useMediaQuery("(min-width: 1060px)");
   return (
-    <section>
+    <section
+      id="home"
+      className="md:flex md:justify-between md:items-center gap-16 md:h-full py-10"
+      >
     {/* IMAGE SECTION */}
     <div className="basis-3/5 z-10 mt-16 md:mt-32 flex justify-center md:order-2">
     {isAboveLarge ? (
@@ -19,11 +23,40 @@ const Landing = ({ setSelectedPage }) => {
     ) : (
       <img
         alt="profile"
-        className="z-10 w-full max-w-[400px] md:max-w-[600px] rounded-t-full"
+        className="z-10 w-full max-w-[400px] md:max-w-[600px] rounded-full"
         src="assets/profile-photo.png"
       />
+      
     )}
   </div>
+                  
+  <div className="z-30 basis-2/5 mt-12 md:mt-32">
+        {/* HEADINGS */}
+        <motion.div
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, x: -50 },
+            visible: { opacity: 1, x: 0 },
+          }}
+        >
+          <p className="text-6xl font-playfair z-10 text-center md:text-start">
+            Chandan {""}
+            <span
+              className="xs:relative xs:text-deep-blue xs:font-semibold z-20 xs:before:content-brush
+              before:absolute before:-left-[25px] before:-top-[70px] before:z-[-1]"
+            >
+              Basak
+            </span>
+          </p>
+
+          <p className="mt-10 mb-7 text-center md:text-start">
+            Graphics design & SEO expert.
+          </p>
+        </motion.div>
+        </div>
   </section>
   )
 }
