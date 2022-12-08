6a5db9ed704cac067cfb7cd23ee853aa3e7b29d4
datapirateX <67560590+datapirateX@users.noreply.github.com>
2022-12-08 09:22:05 +0600

    text color

--- src/scenes/Landing.jsx
+++ src/scenes/Landing.jsx
@@ -43,12 +43,13 @@ const Landing = ({ setSelectedPage }) => {
           }}
         >
           <p className="text-6xl font-playfair z-10 text-center md:text-start">
-            Chandan {""}
+            Hello, {""}
+            <br />
             <span
               className="xs:relative xs:text-deep-blue xs:font-semibold z-20 xs:before:content-brush
               before:absolute before:-left-[25px] before:-top-[70px] before:z-[-1]"
             >
-              Basak
+              I'm Chandan Basak
             </span>
           </p>
 
